53a78d20d40f586bf9fd4a5d66e5fedd3dc0cd6f
BuildTools <unconfigured@null.spigotmc.org>
2023-11-30 10:31:05 -0500

    Code Changes

--- regression.cpp
+++ regression.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include "parser.hpp"
-
-#define SIZE 1000000
 
 //  /usr/local/Cellar/llvm/17.0.5/bin/clang++ -o program regression.cpp
 // ./program
-int main() {
 
+// f(N) = g(N) * sqrt(N)
+// Calculates g(N) based off values of N
+int main() {
     std::ifstream input("data.csv");
     aria::csv::CsvParser parser = aria::csv::CsvParser(input);
     std::ofstream csv;
     csv.open("gConst.csv");
     csv << "N,g(N)\n";
-
     bool header = true;
     for (auto &row: parser) {
         if (header) {
@@ -28,9 +26,7 @@ int main() {
         std::string line = std::to_string(N) + "," + std::to_string(gConst) + "\n";
         csv << line;
     }
-
     input.close();
     csv.close();
-
     return 0;
 }
