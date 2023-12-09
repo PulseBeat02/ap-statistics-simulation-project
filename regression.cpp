93fdaab2724351297b74171b5c9950c46b33d20d
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 22:36:42 -0500

    Code Changes

--- regression.cpp
+++ regression.cpp
@@ -3,6 +3,14 @@
 #include <string>
 #include "parser.hpp"
 
+const double E = 2.71828182845904523536;
+
+inline long double calculateSquareRootGConst(double N, double count);
+
+inline long double calculateExponentGConst(double N, double count);
+
+inline long double calculateProbabilityFromAverage(double N, double count);
+
 //  /usr/local/Cellar/llvm/17.0.5/bin/clang++ -o program regression.cpp
 // ./program
 
@@ -12,7 +20,7 @@ int main() {
     std::ifstream input("data.csv");
     aria::csv::CsvParser parser = aria::csv::CsvParser(input);
     std::ofstream csv;
-    csv.open("gConst.csv");
+    csv.open("const.csv");
     csv << "N,g(N)\n";
     bool header = true;
     for (auto &row: parser) {
@@ -22,11 +30,24 @@ int main() {
         }
         double N = std::stod(row.front());
         double count = std::stod(row.back());
-        const double gConst = count / std::sqrt(N);
-        std::string line = std::to_string(N) + "," + std::to_string(gConst) + "\n";
+        const long double constant = calculateProbabilityFromAverage(N, count);
+        std::string line = std::to_string(N) + "," + std::to_string(constant) + "\n";
         csv << line;
     }
     input.close();
     csv.close();
     return 0;
+}
+
+inline long double calculateExponentGConst(double N, double count) {
+    double first = count / std::sqrt(N);
+    return first / std::pow(count, -0.01 * E);
+}
+
+inline long double calculateSquareRootGConst(double N, double count) {
+    return count / std::sqrt(N);
+}
+
+inline long double calculateProbabilityFromAverage(double N, double count) {
+    return 1 / count;
 }
